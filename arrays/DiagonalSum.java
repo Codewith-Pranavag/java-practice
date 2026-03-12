2b675568bca96a95b45feeebf2b44b025208e253
Codewith-Pranavag <agrawalpranav087@gmail.com>
2026-03-12 09:12:21 +0530

    Add files via upload
    Java program to calculate sum of all diagonal elements of a 2 D matrix

---
+++ arrays/DiagonalSum.java
@@ -0,0 +1,12 @@
+//to print spiral of a matrix
+import java.util.*;
+public class DiagonalSum{
+    public static void main(String args[]){
+        int arr[][]={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i][i];
+        }
+        System.out.print(sum);
+    }
+}
