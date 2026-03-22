7708d8297ca63b72c62552bdd6039bbafa309d26
Codewith-Pranavag <agrawalpranav087@gmail.com>
2026-03-22 08:22:03 +0530

    Add files via upload

---
+++ arrays/searchInsert.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int res=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+               return i;
+
+            }
+            else if(nums[i]!=target && nums[i]<target){
+                res=i;
+            }
+
+        }
+        return res+1;
+        
+    }
+}
