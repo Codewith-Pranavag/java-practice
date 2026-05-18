b33b06ec4ff4eed6cb324eb7a085bd996cfe63bd
PRANAV KUMAR AGRAWAL <agrawalpranav087@gmail.com>
2026-05-18 17:37:42 +0530

    Create maxProduct.java
    152. Maximum Product Subarray
    
    Given an integer array nums, find a subarray that has the largest product, and return the product.
    
    The test cases are generated so that the answer will fit in a 32-bit integer.
    
    Note that the product of an array with a single element is the value of that elemen

---
+++ arrays/maxProduct.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int left=1;
+        int right=1;
+        int ans=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(left==0) left=1;
+            if(right==0) right=1;
+            left*=nums[i];
+            right*=nums[nums.length-i-1];
+            ans=Math.max(ans,Math.max(left,right));
+        }
+        return ans;
+
+        
+    }
+}
