c76f02357bfcc1cc98c915c637d422bcf4cdb7f0
PRANAV KUMAR AGRAWAL <agrawalpranav087@gmail.com>
2026-05-21 08:56:19 +0530

    Create MyLinkedList.java
    707. Design Linked List
    
    Design your implementation of the linked list. You can choose to use a singly or doubly linked list.
    A node in a singly linked list should have two attributes: val and next. val is the value of the current node, and next is a pointer/reference to the next node.
    If you want to use the doubly linked list, you will need one more attribute prev to indicate the previous node in the linked list. Assume all nodes in the linked list are 0-indexed.
    
    Implement the MyLinkedList class:
    
    MyLinkedList() Initializes the MyLinkedList object.
    int get(int index) Get the value of the indexth node in the linked list. If the index is invalid, return -1.
    void addAtHead(int val) Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
    void addAtTail(int val) Append a node of value val as the last element of the linked list.
    void addAtIndex(int index, int val) Add a node of value val before the indexth node in the linked list. If index equals the length of the linked list, the node will be appended to the end of the linked list. If index is greater than the length, the node will not be inserted.
    void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.

---
+++ LinkedList/MyLinkedList.java
@@ -0,0 +1,104 @@
+class MyLinkedList {
+    class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int val){
+            this.val=val;
+            next=null;
+        }
+    }
+    ListNode head;
+    ListNode tail;
+
+    public MyLinkedList() {
+        head=null;
+        tail=null;
+        
+    }
+
+    public int get(int index) {
+        int i=0;
+        ListNode temp=head;
+        while(temp!=null){
+            if(index==i){
+                return temp.val;
+            }
+            temp=temp.next;
+            i++;
+        }
+        return -1;
+    }
+    
+    public void addAtHead(int val) {
+        ListNode newnode=new ListNode(val);
+        if(head==null){
+            head=tail=newnode;
+            return;
+        }
+        newnode.next=head;
+        head=newnode;
+    }
+    
+    public void addAtTail(int val) {
+        ListNode newnode=new ListNode(val);
+        if(head==null){
+            head=tail=newnode;
+            return;
+        }
+        tail.next=newnode;
+        tail=newnode;
+        
+    }
+    
+    public void addAtIndex(int index, int val) {
+        if(index==0){
+            addAtHead(val);
+            return;
+        }
+        int size=0;
+        ListNode temp=head;
+        while(temp!=null){
+            temp=temp.next;
+            size++;
+        }
+        if(index>size) return;
+        if(size==index){
+            addAtTail(val);
+            return;
+        }
+        ListNode newnode=new ListNode(val);
+        int i=0;
+        temp=head;
+        while(i!=index-1){
+            temp=temp.next;
+            i++;
+        }
+        newnode.next=temp.next;
+        temp.next=newnode;
+    }
+    
+    public void deleteAtIndex(int index) {
+        if(head==null){
+            return;
+        }
+        if(index==0){
+            head=head.next;
+            if(head==null){
+                tail=null;
+            }
+            return;
+        }
+        ListNode prev=head;
+
+        int i=0;
+        while(prev.next!=null && i<index-1){
+            prev=prev.next;
+            i++;
+        }
+        if(prev.next==null) return;
+        if(prev.next==tail){
+            tail=prev;
+        }
+        prev.next=prev.next.next;
+    }
+}
