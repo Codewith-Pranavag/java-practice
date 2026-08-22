145a16a5491582fc4679bd455a08a7579908af06
PRANAV KUMAR AGRAWAL <agrawalpranav087@gmail.com>
2026-08-22 11:30:59 +0530

    Create singleNumber.java
    Single Number III
    
    Given an integer array nums, in which exactly two elements appear only once and all the other elements appear exactly twice. Find the two elements that appear only once. You can return the answer in any order.
    
    You must write an algorithm that runs in linear runtime complexity and uses only constant extra space.

---
+++ hash/singleNumber.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        ArrayList<Integer> l=new ArrayList<>();
+        HashMap<Integer,Integer > map=new HashMap<>();
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        } 
+        for(Integer key: map.keySet()){
+            if(map.get(key)==1) l.add(key);
+        }
+        return l.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
