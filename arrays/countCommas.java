f9755c0629863df0334f6f7d25914ccbda10a065
PRANAV KUMAR AGRAWAL <agrawalpranav087@gmail.com>
2026-09-09 06:39:41 +0530

    Create countCommas.java
    3871. Count Commas in Range II

---
+++ arrays/countCommas.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        long comma=0;
+        long min=1000;
+        while(n>=min){
+            comma+=(n-min+1);
+            min=min*1000;
+        }
+        return comma;
+    }
+}
