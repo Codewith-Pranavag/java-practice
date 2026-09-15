ab16160745d39a595372ab33427e816bd39a18e1
PRANAV KUMAR AGRAWAL <agrawalpranav087@gmail.com>
2026-09-15 20:24:32 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("this is Pranav Kumar Agrawal");
+        System.out.println(" Pranav Kumar Agrawal");
     }
 }
